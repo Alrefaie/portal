676ae567fce7ceb501cb5f0d8ded6e5b796568a7
Alrefaie <74877036+Alrefaie@users.noreply.github.com>
2021-01-13 13:39:18 +0100

    Update __init__.py

--- zoom_integration/models/__init__.py
+++ zoom_integration/models/__init__.py
@@ -1,3 +1,13 @@
 from . import res_users
 from . import calendar
 from . import company
+"""Python wrapper around the Zoom.us REST API"""
+
+from __future__ import absolute_import, unicode_literals
+
+from zoomus.client import ZoomClient
+from zoomus.util import API_VERSION_1, API_VERSION_2
+
+
+__all__ = ["API_VERSION_1", "API_VERSION_2", "ZoomClient"]
+__version__ = "1.1.3"
